74faba54c9ea3b96bdb9c8f6519a4aeb63393e74
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-09-03 17:58:14 -0300

    findByID implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.ISellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements  ISellerDAO {
-
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,9 +39,45 @@ public class SellerDaoJDBC implements  ISellerDAO {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	    PreparedStatement st = null;
+	    ResultSet rs = null;
+
+	    try {
+	        st = conn.prepareStatement(
+	                "SELECT seller.*, department.Name as DepName " +
+	                "FROM seller " +
+	                "INNER JOIN department ON seller.DepartmentId = department.Id " +
+	                "WHERE seller.Id = ?");
+	        st.setInt(1, id);
+	        rs = st.executeQuery();
+
+	        if (rs.next()) {
+	            Department dep = new Department();
+	            dep.setId(rs.getInt("DepartmentId")); // Correção: Use o nome correto da coluna
+	            dep.setName(rs.getString("DepName"));
+
+	            Seller obj = new Seller();
+	            obj.setId(rs.getInt("Id")); // Correção: Use o nome correto da coluna
+	            obj.setName(rs.getString("Name"));
+	            obj.setEmail(rs.getString("Email"));
+	            obj.setBaseSalary(rs.getDouble("BaseSalary"));
+	            obj.setBirthDate(rs.getDate("BirthDate"));
+	            obj.setDepartment(dep);
+
+	            return obj;
+	        }
+	    } catch (SQLException e) {
+	        throw new DbException(e.getMessage());
+	    } finally {
+	        DB.closeStatement(st);
+	        DB.closeResultSet(rs);
+	    }
+
+	    return null;
 	}
+
+		
+		
 
 	@Override
 	public List<Seller> findAll() {
